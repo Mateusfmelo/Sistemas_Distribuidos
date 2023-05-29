35970fd28eed137487ec5841553d5c2c60b5b272
Mateus Melo <62157751+Mateusfmelo@users.noreply.github.com>
2023-05-29 19:31:57 -0300

    Update client.py

--- client.py
+++ client.py
@@ -2,9 +2,9 @@ import rpyc
 
 conn = rpyc.connect("localhost", 18861) # Substitua o LocalHost pelo endereço IPV4 da rede Wi-Fi e execute
 
-a = 10
-b = 5
-c = 3
+a = int(input("Digite um valor: "))
+b = int(input("Digite um valor: "))
+c = int(input("Digite um valor: "))
 
 result = conn.root.sum_and_divide(a, b, c)
 # Imprime o resultado da soma de a e b dividido por c
